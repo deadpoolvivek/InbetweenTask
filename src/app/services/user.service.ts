6a4ed9992fb4e9b3863941b6e4a81f36d5f137f8
deadpoolvivek <36727510+deadpoolvivek@users.noreply.github.com>
2019-06-29 01:15:25 +0530

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,18 +11,19 @@ export class UserService {
 
   constructor(private http: HttpClient, private errorHandler: ErrorHandlerService) { }
 
+  // Service to get 20 records
   geUser() {
     return this.http.get(ApplicationUrls.user.getUser + '?results=20').pipe(
       tap((u: any) => {
         console.log(u.results);
+        //result is stored in users array
         this.users = u.results;
       }),
       catchError(this.errorHandler.handleError('getSchool', [])));
   }
 
 
-// Service to get the selected user data
-// Data  stored in the servie as it gets changes randomly
+  // Service to get the selected user data
   getUserById(id: string) {
     return this.users.find( u => u.login.uuid === id);
   }
