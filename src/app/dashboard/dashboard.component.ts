4cc6f3fe8fe4f21b3e28dcf25cc329ef5efe778f
deadpoolvivek <36727510+deadpoolvivek@users.noreply.github.com>
2019-06-29 01:21:35 +0530

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,19 +11,20 @@ import { User } from '../models/user.model';
 export class DashboardComponent implements OnInit {
   listUser: any;
   listUserDragged: any;
-  user: User; // user model
+  user: User; // user is type of User model
 
   constructor(private userService: UserService, private router: Router, ) {
     this.user = new User();
   }
 
   ngOnInit() {
+    //function call to get 20 user records
     this.getUSers();
   }
 
-  // drop function
+  // drop function to drop rows on movement
   drop(event: CdkDragDrop<string[]>) {
-    console.log(event);
+    //console.log(event);
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -34,15 +35,16 @@ export class DashboardComponent implements OnInit {
     }
   }
 
-  // function to get the list of users
+  // function to subscribe or get the list of 20 users from service
   getUSers() {
     this.userService.geUser().subscribe((res: any) => {
+      //result or subscribed data is stoed in listUser variable for use 
       this.listUser = res.results;
-      console.log(res);
+     // console.log(res);
     });
   }
 
-  // Function to redirect to the profile page
+  // Function to redirect to the profile page(user details page) with user id
   detailUser(event) {
     this.router.navigate(['profile/', event.login.uuid]);
   }
